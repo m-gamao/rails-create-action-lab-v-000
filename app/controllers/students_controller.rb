e056ef854aefff89ee79efa058e7c73f8485b4e4
Gigi Gamao <mmgamao1@gmail.com>
2019-11-24 19:31:06 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,22 @@
 class StudentsController < ApplicationController
-  
   def index
     @students = Student.all
   end
-
+#/students/1
   def show
     @student = Student.find(params[:id])
   end
-
+#/students/new  
   def new
   end
 
   def create
+    @student = Student.create(
+      {
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+    )
+    redirect_to student_path(@student)
   end
-
 end
